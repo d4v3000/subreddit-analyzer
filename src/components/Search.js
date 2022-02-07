ae06cb2233145d289e98c0b9675b8c832231acef
David Glogaza <david.glogaza40@googlemail.com>
2022-02-07 15:39:22 +0100

    add search form

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,9 +2,11 @@ import React from "react";
 import SearchForm from "./SearchForm";
 
 function Search() {
+  const onSearch = () => {};
+
   return (
     <div className="container items-center mx-auto">
-      <SearchForm />
+      <SearchForm onSearch={onSearch} />
     </div>
   );
 }
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,11 +1,40 @@
 import React from "react";
+import { useState } from "react";
 
-function SearchForm() {
+function SearchForm({ onSearch }) {
+  const [subreddit, setSubreddit] = useState("javascript");
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    onSearch(subreddit);
+  };
+
   return (
     <div className="mx-96 pt-8">
       <h1 className="text-5xl mx-4 pt-1 font-display tracking-widest pl-7 mt-1 leading-relaxed">
         Find the best time for a subreddit
       </h1>
+
+      <form onSubmit={onSubmit} className="mx-20">
+        <label htmlFor="subreddit" className="mx-4">
+          r/
+        </label>
+        <input
+          type="text"
+          name="subreddit"
+          value={subreddit}
+          onChange={(event) => {
+            setSubreddit(event.target.value);
+          }}
+          className="shadow appearance-none border rounded w-80 py-2 px-3 text-gray-700 leading-tight focus:outline-none"
+        />
+        <button
+          type="submit"
+          className="bg-orange-100 text-sm font-mono ml-12 mt-11 pt-2 pb-2 px-4 text-white rounded leading-tight"
+        >
+          Search
+        </button>
+      </form>
     </div>
   );
 }
