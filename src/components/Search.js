50cad28f034cbb74e7701ba1c86d8bf016864451
David Glogaza <david.glogaza40@googlemail.com>
2022-02-07 15:23:20 +0100

    add form header

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,12 @@
 import React from "react";
+import SearchForm from "./SearchForm";
 
 function Search() {
-  return <div></div>;
+  return (
+    <div className="container items-center mx-auto">
+      <SearchForm />
+    </div>
+  );
 }
 
 export default Search;
---
+++ src/components/SearchForm.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function SearchForm() {
+  return (
+    <div className="mx-96 pt-8">
+      <h1 className="text-5xl mx-4 pt-1 font-display tracking-widest pl-7 mt-1 leading-relaxed">
+        Find the best time for a subreddit
+      </h1>
+    </div>
+  );
+}
+
+export default SearchForm;
