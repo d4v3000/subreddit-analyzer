73d48dd7b176f5f2e335b03bec667d1c0e9db130
David Glogaza <david.glogaza40@googlemail.com>
2022-02-08 11:49:14 +0100

    add fetching top reddit posts

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,42 @@
 import React from "react";
 import SearchForm from "./SearchForm";
+import { useState, useEffect } from "react";
 
 function Search() {
-  const onSearch = () => {};
+  const [posts, setPosts] = useState([]);
+  const [status, setStatus] = useState("idle");
+  const [after, setAfter] = useState("");
+  const [subreddit, setSubreddit] = useState("");
+
+  useEffect(() => {
+    if (posts.length < 500 && subreddit) {
+      fetchData();
+    }
+  }, [subreddit, after]);
+
+  async function fetchData() {
+    setStatus("loading");
+    const url = `https://www.reddit.com/r/${subreddit}/top.json?t=all&limit=100&after=${after}`;
+    const response = await fetch(url);
+    const { data } = await response.json();
+    data.children.forEach((child) => {
+      setPosts((posts) => [...posts, child]);
+    });
+    setAfter(data.after);
+    setStatus("resolved");
+  }
 
   return (
     <div className="container items-center mx-auto">
-      <SearchForm onSearch={onSearch} />
+      <SearchForm onSearch={setSubreddit} setPosts={setPosts} />
+
+      {status === "loading" && (
+        <div className="mx-96 my-20">
+          <div className="mx-72 w-8 h-8 border-b-2 border-gray-900 rounded-full animate-spin"></div>
+        </div>
+      )}
+
+      {status === "resolved" && <p>{posts.length}</p>}
     </div>
   );
 }
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { useState } from "react";
 
-function SearchForm({ onSearch }) {
+function SearchForm({ onSearch, setPosts }) {
   const [subreddit, setSubreddit] = useState("javascript");
 
   const onSubmit = (event) => {
     event.preventDefault();
     onSearch(subreddit);
+    setPosts([]);
   };
 
   return (
